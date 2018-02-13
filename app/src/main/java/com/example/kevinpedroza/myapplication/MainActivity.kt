dc300a00b490d35c8e2738e47c3e34c317838291
KEVIN_PEDROZA <kevin.andres01@hotmail.com>
2018-02-12 23:00:22 -0500

    Segundo commit

---
+++ app/src/main/java/com/example/kevinpedroza/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kevinpedroza.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //Clase bundle facilita que al rotar el telefono no se pierdan los datos de la actividad vertical a la horizontal
+        super.onCreate(savedInstanceState)
+        //La clase R se crea por default, la cual permite obtener los recursos (resource) para usarlos en el Layout. De Layout invocamos el archivo activity_main.xml
+        setContentView(R.layout.activity_main)
+
+    }
+}
